cb3b2c92fe9f4bcaaa89897be5485a76fe7f614e
Pamela Luque <pamela_luque@mckinsey.com>
2023-11-10 15:23:25 -0500

    chore: add vnet module

---
+++ modules/vnet/main.tf
@@ -0,0 +1,8 @@
+# vnet_module/main.tf
+
+resource "azurerm_virtual_network" "vnet-module" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
---
+++ modules/vnet/variables.tf
@@ -0,0 +1,21 @@
+# vnet_module/variables.tf
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "Address space of the virtual network"
+  type        = list(string)
+}
+
+variable "location" {
+  description = "Azure region where the virtual network will be created"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
